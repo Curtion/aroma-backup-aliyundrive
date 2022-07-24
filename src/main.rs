f5684724f0dd070f26764b67167fccec30e20309
Curtion <curtion@126.com>
2022-07-24 23:46:15 +0800

    feat: 执行命令

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::path::Path;
+use std::process::Command;
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(env::current_dir().unwrap().as_os_str())
+        .join("aliyundrive")
+        .join("alidrive");
+    match Command::new(path).arg("README.md").arg("GTNH").output() {
+        Ok(output) => {
+            println!("{}", String::from_utf8_lossy(&output.stdout));
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
 }
