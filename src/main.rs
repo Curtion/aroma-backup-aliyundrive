72e9842c50fde157ef80f04c193ab7cfdcbf84dc
Curtion <curtion@126.com>
2022-07-25 23:19:31 +0800

    chore: fn

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use std::env;
 use std::path::Path;
 use std::process::Command;
 fn main() {
+    upload("README.md");
+}
+
+fn upload(name: &str) {
     let path = Path::new(env::current_dir().unwrap().as_os_str())
         .join("aliyundrive")
         .join("alidrive");
-    match Command::new(path).arg("README.md").arg("GTNH").output() {
+    match Command::new(path).arg(name).arg("GTNH").output() {
         Ok(output) => {
             println!("{}", String::from_utf8_lossy(&output.stdout));
         }
