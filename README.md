3ca9f4cd10fd0bc430e00f00a80d0987f5692c4a
Curtion <curtion@126.com>
2022-07-30 22:11:55 +0800

    feat: 完成基本功能

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# TODO
+# 介绍
 
 我的世界`AromaBackup`备份，自动上传到阿里云盘。
+
+自动获取最新的备份文件，然后上传到阿里云盘。
+
+# 使用
+
+下载`aroma-backup-aliyundrive`放进`backups`路径。
+
+下载 https://github.com/aoaostar/alidrive-uploader 连同配置文件一起放进`backups/aliyundrive`文件夹
+
+然后定时执行`aroma-backup-aliyundrive`即可。
+
+# 备注
+
+`aroma-backup-aliyundrive`为`x86_64-Linux`版本，其它系统可执行文件需要自行编译。
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,82 @@
 use std::env;
-use std::fs::{File,OpenOptions};
-use std::io::Write;
+use std::fs::File;
+use std::io::Read;
 use std::path::Path;
 use std::process::Command;
 fn main() {
-    create_file_log();
-    let mut file =  OpenOptions::new().append(true).open("upload.log").expect("open file error");
-    upload("README.md",&mut file);
+    let last_name = get_will_last_filename();
+    let path = file2name(&last_name);
+    let pathfile = Path::new(&path).join(last_name);
+    upload(pathfile.as_os_str().to_str().unwrap());
 }
 
-fn upload(name: &str,file: &mut File) {
+fn upload(name: &str) {
     let path = Path::new(env::current_dir().unwrap().as_os_str())
         .join("aliyundrive")
         .join("alidrive");
     match Command::new(path).arg(name).arg("GTNH").output() {
-        Ok(output) => {
-            file.write_all(String::from_utf8_lossy(&output.stdout).as_bytes()).expect("write file error");
-            file.write_all(b"..............................................................................................................................................").expect("write file error");
+        Ok(_) => {
+            println!("提交上传任务成功");
         }
         Err(e) => {
-            file.write_all(e.to_string().as_bytes()).expect("write file error");
+            println!("{}", e);
         }
     }
 }
 
-fn create_file_log() {
-    let is_exists = Path::new("upload.log").exists();
-    if !is_exists {
-        let mut file = OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .open("upload.log")
-            .unwrap();
-        file.write_all(b"").unwrap();
+fn get_will_last_filename() -> String {
+    let path = Path::new(env::current_dir().expect("获取当前路径失败").as_os_str())
+        .join("World")
+        .join("backupstore.txt");
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("{}", e);
+            return String::new();
+        }
+    };
+    let mut content = String::new();
+    file.read_to_string(&mut content)
+        .expect("读取backupstore.txt文件失败");
+    let lines = content.split("\r\n");
+    let mut vesc = lines.collect::<Vec<&str>>();
+    vesc.pop();
+    match vesc.pop() {
+        Some(last) => equal2reduce(last),
+        None => {
+            println!("文件内容为空");
+            String::new()
+        }
     }
+}
+
+fn equal2reduce(name: &str) -> String {
+    //World=2022=7=29=22=59"
+    let vesc = name.split("=").collect::<Vec<&str>>();
+    let mut name = "Backup".to_string();
+    name.push('-');
+    name.push_str(vesc[0]);
+    name.push('-');
+    name.push_str(vesc[1]);
+    name.push('-');
+    name.push_str(vesc[2]);
+    name.push('-');
+    name.push_str(vesc[3]);
+    name.push_str("--");
+    name.push_str(vesc[4]);
+    name.push('-');
+    name.push_str(vesc[5]);
+    name.push_str(".zip");
+    name
+}
+
+fn file2name(filename: &str) -> String {
+    //Backup-World-2022-7-29--22-59.zip
+    let keys = filename.split("-").collect::<Vec<&str>>();
+    let filepath = Path::new(env::current_dir().expect("获取当前路径失败").as_os_str())
+        .join(keys[1])
+        .join(keys[2])
+        .join(keys[3])
+        .join(keys[4]);
+    filepath.to_str().expect("解析路径失败").to_string()
 }
